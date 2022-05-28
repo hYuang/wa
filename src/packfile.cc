48aaa256e1b1a6fd682c83b63808b7da8774d1bf
hYang <familyhuangyang@hotmail.com>
2022-05-29 00:53:43 +0800

    wasm demo about msg_loop main packfile

---
+++ src/packfile.cc
@@ -0,0 +1,16 @@
+#include <emscripten.h>
+#include <stdio.h>
+
+int main () {
+    FILE* fp = fopen("hello.txt","rt");
+    if(fp) {
+        while(!feof(fp)) {
+            char c = fgetc(fp);
+            if ( c != EOF) {
+                putchar(c);
+            }
+        }
+        fclose(fp);
+    }
+    return 0;
+}
